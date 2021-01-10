c716b78b152f2c17b4cd04ca2bc9ed354a6670c2
Sachin Bhale <sachinbhale38@gmail.com>
2021-01-10 18:31:18 +0530

    changes in controller

--- SpringBoot-crud-Restful-WebServices/src/main/java/com/ingecno/controller/UserController.java
+++ SpringBoot-crud-Restful-WebServices/src/main/java/com/ingecno/controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.DispatcherServlet;
 
 import com.ingecno.entity.Country;
 import com.ingecno.entity.UserDetails;
